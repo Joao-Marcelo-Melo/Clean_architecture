da24b3931a1d7dde39411ef4e7a5b11e562f2c06
joao-marcelo-melo <marcelo25256464@gmail.com>
2023-09-24 15:22:04 -0300

    feat: Implementing select and tests in UsersRepository

--- src/infra/db/repositories/users_repository_test.py
+++ src/infra/db/repositories/users_repository_test.py
@@ -1,10 +1,59 @@
-# from src.infra.db.settings.connection import DBConnectionhandler
+import pytest
+from sqlalchemy import text
+from src.infra.db.settings.connection import DBConnectionhandler
 from .users_repository import UsersRepository
 
+db_connection_handler = DBConnectionhandler()
+connection = db_connection_handler.get_engine().connect()
+
+@pytest.mark.skip(reason='Sensive Test')
 def test_insert_user():
-    mocked_first_name = 'joao'
-    mocked_last_name = 'marcelo'
-    mocked_age = 18
+    mocked_first_name = 'first_name'
+    mocked_last_name = 'last_name'
+    mocked_age = 19
 
     users_repository = UsersRepository()
     users_repository.insert_user(mocked_first_name, mocked_last_name, mocked_age)
+
+    sql = '''
+        SELECT  * FROM  users
+        WHERE first_name = '{}'
+        AND last_name = '{}'
+        AND age = ' {} '   
+    '''.format(mocked_first_name, mocked_last_name, mocked_age)
+    response = connection.execute(text(sql))
+    registry = response.fetchall()[0]
+
+    assert registry.first_name == mocked_first_name
+    assert registry.last_name == mocked_last_name
+    assert registry.age == mocked_age
+
+    connection.execute(text(f'''
+        DELETE FROM users WHERE id = {registry.id}
+    '''))
+    connection.commit()
+
+@pytest.mark.skip(reason='Sensive Test')
+def test_select_user():
+    mocked_first_name = 'first_name_2'
+    mocked_last_name = 'last_name_2'
+    mocked_age = 19
+
+    sql = '''
+        INSERT INTO  users (first_name, last_name, age) VALUES ('{}', '{}', '{}')
+    '''.format(mocked_first_name, mocked_last_name, mocked_age)
+
+    connection.execute(text(sql))
+    connection.commit()
+
+    users_repository = UsersRepository()
+    response = users_repository.select_user(mocked_first_name)
+
+    assert response[0].first_name == mocked_first_name
+    assert response[0].last_name == mocked_last_name
+    assert response[0].age == mocked_age
+
+    connection.execute(text(f'''
+        DELETE FROM users WHERE id = {response[0].id}
+    '''))
+    connection.commit()
